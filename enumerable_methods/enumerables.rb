8c35d4f21bc06e9ee9ee31b0743a6ae3c8b54dee
Nathan Struhs <nathanstruhs@gmail.com>
2016-12-10 11:17:03 -0800

    Complete inject method

--- enumerable_methods/enumerables.rb
+++ enumerable_methods/enumerables.rb
@@ -1,47 +1,95 @@
 module Enumerable
-	
-  def my_each
-  	array = self.to_a
-  	(array.length).times do |index|
-  		yield(array[index])
-  	end
+
+  def my_each 
+    return self.to_enum unless block_given?
+    array = self.to_a 
+    n = array.length 
+    n.times do |i|
+      yield array[i]
+    end
   end
 
   def my_each_with_index
-  	array = self.to_a
-  	(array.length).times do |index|
-  		yield(array[index], index)
-  	end
+    return self.to_enum unless block_given?
+    array = self.to_a
+    offset = 1
+    array.my_each { |i| yield(array[i-offset], i-offset) }
   end
 
-  # def my_select
-  # end
+  def my_select
+    return self.to_enum unless block_given?
+    result = []
+    self.my_each { |i| result << i if yield(i) }
+    return result
+  end
 
-  # def my_all?
-  # end
+  def my_all?
+    if block_given?
+      my_each { |i| return false unless yield(i) }
+    else
+      my_each { |i| return false unless i }
+    end
+    true
+  end
 
-  # def my_any?
-  # end
+  def my_any?
+    if block_given?
+      my_each { |i| return true if yield(i) }
+    else
+      my_each { |i| return true if i }
+    end
+    false
+  end
 
-  # def my_none?
-  # end
+  def my_none?
+    if block_given?
+      my_each { |i| return false if yield(i) }
+    else
+      my_each { |i| return false if i }
+    end
+    true
+  end
 
-  # def my_count
-  # end
+  def my_count
+    count = 0
+    my_each { |i| i += 1 }
+    count = i
+  end
 
-  # def my_map
-  # end
+  def my_map
+    return self.to_enum unless block_given?
+    array = []
+    my_each { |i| array << yield(i) }
+    array
+  end
 
-  # def my_inject
-  # end
+  def my_inject(num=0)
+    accumulator = num
+    self.my_each {|i| accum=yield(accum,i)}
+    accum
+  end
 
 end
 
-numbers = [1,2,3,4]
+numbers = [1,2,3,4,5,6]
 
 numbers.my_each { |i| puts i * 3 }
 
-numbers.my_each_with_index { |element, index| puts "Value of #{element} at index #{index}" }
+numbers.my_each_with_index { |element, index| puts "Element: #{element}, index: #{index}" }
+
+evens = numbers.my_select { |i| i % 2 == 0 }
+print evens 
+puts
+
+puts numbers.my_all? { |number| number < 7 }
+
+puts numbers.my_any? { |number| number > 5 }
+
+puts numbers.my_none? { |number| number > 6 }
+
+puts numbers.count { |number| number % 2 == 0 }
+
+puts numbers.my_map { |number| number * 3 }
 
 
 
